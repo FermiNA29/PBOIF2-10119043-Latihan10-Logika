be915034c003b362728211ac8f023c0ea988190e
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2020-10-18 18:54:51 +0700

    Tugas PBO logika

---
+++ src/pboif2/pkg10119043/latihan10/logika/PBOIF210119043Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119043.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA : FERMI NAUFAL AKBAR
+ * KELAS     : IF2 
+ * NIM       : 10119043 
+ * DESKRIPSI : Program ini berisi program logika dengan tipe data boolean
+ */
+public class PBOIF210119043Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
